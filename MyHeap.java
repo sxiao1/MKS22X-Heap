8518065f53c9f286d072febb7820e37fbad6c5a5
sxiao1 <sxiao1@stuy.edu>
2019-04-28 18:16:34 -0400

    working on pushdown

---
+++ MyHeap.java
@@ -0,0 +1,30 @@
+public class MyHeap{
+  private static void pushDown(int[]data,int size,int index){
+    if((index * 2 + 1 < size && data[index] < data[index * 2 + 1]) || (index * 2 + 2 < size && data[index] < data[index * 2 + 2])){
+      if(data[index * 2 + 2] > data[index * 2 + 1]){
+        int temp = data[index * 2 + 2];
+        data[index * 2 + 2] = data[index];
+        data[index] = temp;
+      }
+      else{
+        int temp = data[index * 2 + 1];
+        data[index * 2 + 1] = data[index];
+        data[index] = temp;
+      }
+    }
+    else{
+      if(index *2 + 1 <size){
+        pushDown(data, size, index + 1);
+      }
+    }
+  }
+  public static void main(String[]args){
+    int[] nums = {13,2,34,12,5,6,29,70,88};
+    pushDown(nums, 9, 0);
+    String s = "";
+    for(int i = 0; i < nums.length; i++){
+      s += nums[i] + ", ";
+    }
+    System.out.println(s);
+  }
+}
