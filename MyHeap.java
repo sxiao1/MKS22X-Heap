ed4f771f2fff3763ac9871e4c5c1704f4b4d8d21
sxiao1 <sxiao1@stuy.edu>
2019-04-28 18:29:11 -0400

    got the basics done for pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,16 +1,21 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
     if((index * 2 + 1 < size && data[index] < data[index * 2 + 1]) || (index * 2 + 2 < size && data[index] < data[index * 2 + 2])){
+      int greater = 0;
       if(data[index * 2 + 2] > data[index * 2 + 1]){
+        greater = index * 2 + 2;
         int temp = data[index * 2 + 2];
         data[index * 2 + 2] = data[index];
         data[index] = temp;
       }
       else{
+        greater = index * 2 + 1;
         int temp = data[index * 2 + 1];
         data[index * 2 + 1] = data[index];
         data[index] = temp;
       }
+      pushDown(data, size, greater);
+      pushDown(data,size, index + 1);
     }
     else{
       if(index *2 + 1 <size){
