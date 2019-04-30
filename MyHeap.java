7f998a4cc919948520f84c2525a7148c572c86f0
sxiao1 <sxiao1@stuy.edu>
2019-04-29 22:58:28 -0400

    added heapsort and pushUp

--- MyHeap.java
+++ MyHeap.java
@@ -2,8 +2,13 @@ public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
     if((index * 2 + 1 < size && data[index] < data[index * 2 + 1]) || (index * 2 + 2 < size && data[index] < data[index * 2 + 2])){
       int greater = 0;//finding the greater index to swap with
-      //if(index * 2 + 2 == size - 1)
-      if(data[index * 2 + 2] > data[index * 2 + 1]){
+      if(index * 2 + 2 >= size){
+        greater = index * 2 + 1;
+        int temp = data[index * 2 + 1];
+        data[index * 2 + 1] = data[index];
+        data[index] = temp;
+      }
+      else if(data[index * 2 + 2] > data[index * 2 + 1]){
         greater = index * 2 + 2;
         int temp = data[index * 2 + 2];//switching the values
         data[index * 2 + 2] = data[index];
@@ -25,21 +30,44 @@ public class MyHeap{
     }
   }
   private static void pushUp(int[]data,int index){
-    if(((index - 1) / 2 >= 0 && data[index] > data[(index - 1) / 2]) || ((index - 2) / 2 >= 0 && data[index] > data[(index -2)/2])){
-      int greater = 0;
-      if(data[(index - 2) / 2] > data[(index - 1)/2]){
-
+    if(((index - 1) / 2 >= 0 && index != 0 && data[index] >= data[(index - 1) / 2]) ){
+    while(((index - 1) / 2 >= 0 && index != 0 && data[index] >= data[(index - 1) / 2]) ){
+      int temp = data[(index -1) / 2];
+      data[(index - 1)/ 2] = data[index];
+      data[index] = temp;
+      index = (index - 1) / 2;
+    }
+  }
+    else{
+      while(((index - 1) / 2 >= 0 && index != 0 && data[index] < data[(index - 1) / 2])){
+        int temp = data[(index -1) / 2];
+        data[(index - 1)/ 2] = data[index];
+        data[index] = temp;
+        index = (index - 1)/ 2;
       }
     }
+
   }
   public static void heapify(int[] nums){
     int size = nums.length;
     pushDown(nums, size, 0);
     pushDown(nums, size, 0);//twice to run through the branches and make sure they are in the right place
+    //pushUp(nums, nums.length - 1);
+  }
+  public static void heapsort(int[] nums){
+    int[] newNums = new int[nums.length];
+    for(int i = 0; i < nums.length; i++){
+      newNums[i] = nums[i];
+    }
+    heapify(newNums);
+    for(int i = 0; i < nums.length;i++){
+      nums[i] = newNums[i];
+    }
   }
   public static void main(String[]args){
     int[] nums = {13,2,34,12,5,6,29,70,88};
-    heapify(nums);
+    //heapify(nums);
+    heapsort(nums);
     String s = "";
     for(int i = 0; i < nums.length; i++){
       s += nums[i] + ", ";
